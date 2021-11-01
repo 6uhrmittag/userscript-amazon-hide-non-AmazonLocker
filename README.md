5b0ae3da3e27bfed9467f363533d8497ca83c443
Marvin Heimbrodt <marvin.heimbrodt@form4.de>
2021-11-01 21:45:03 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Amazon - hide non Amazon Locker
+
+This UserScript hides products that don't ship to Amazon Locker without limiting the search to Prime.
+
+It's currently tested only on Amazon.de
+
+## Acknowledgments
+
+This script is based on Tad Wohlrapp's well written
+script: https://github.com/tadwohlrapp/amazon-show-seller-info-userscript
+
+
---
+++ userscript-amazon-hide-non-AmazonLocker.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name            Amazon - hide non Amazon Locker
+// @description     Hides Products that don't ship to Amazon Locker, when Amazon Locker is selected as Destination
+// @namespace       https://github.com/6uhrmittag
+// @author          Marvin Heimbrodt
+// @version         0.1
+// @license         MIT
+// @homepageURL     https://github.com/6uhrmittag/userscript-amazon-hide-non-AmazonLocker
+// @supportURL      https://github.com/6uhrmittag/userscript-amazon-hide-non-AmazonLocker/issues
+// @updateURL       https://github.com/6uhrmittag/userscript-amazon-hide-non-AmazonLocker/raw/main/amazon-show-seller-info.meta.js
+// @downloadURL     https://github.com/6uhrmittag/userscript-amazon-hide-non-AmazonLocker/raw/main/amazon-show-seller-info.user.js
+// @match           https://smile.amazon.co.uk/*
+// @match           https://www.amazon.co.uk/*
+// @match           https://smile.amazon.de/*
+// @match           https://www.amazon.de/*
+// @match           https://www.amazon.es/*
+// @match           https://www.amazon.fr/*
+// @match           https://www.amazon.it/*
+// ==/UserScript==
